843f48f1359b4c3816e1ecd08a0f2a682c37351b
Afta Pasma Lohdri <aftaplasma708@gmail.com>
2024-06-04 16:03:40 +0700

    membuat tampilan home dan profile perusahaan

--- app/src/main/java/org/d3if/infoker/ui/screen/perusahaan/tabs/HomeScreen.kt
+++ app/src/main/java/org/d3if/infoker/ui/screen/perusahaan/tabs/HomeScreen.kt
@@ -1,15 +1,23 @@
 package org.d3if.infoker.ui.screen.perusahaan.tabs
 
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -21,16 +29,21 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.checkerframework.checker.units.qual.C
 import org.d3if.infoker.R
 import org.d3if.infoker.ui.theme.InfokerTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -53,58 +66,60 @@ fun HomeScreen() {
                     tint = MaterialTheme.colorScheme.primary
                 )
             }
-        },
-//        bottomBar = {
-//            BottomAppBar(
-//                content = {
-//                    Row(
-//                        modifier = Modifier.fillMaxWidth(),
-//                        horizontalArrangement = Arrangement.Center
-//                    ) {
-//                        IconButton(
-//                            onClick = { },
-//                            modifier = Modifier
-//                                .size(48.dp)
-//                                .weight(1f)
-//                        ) {
-//                            Icon(
-//                                painter = painterResource(id = R.drawable.baseline_bookmark_border_24),
-//                                contentDescription = "Bookmark"
-//                            )
-//                        }
-//                        IconButton(
-//                            onClick = { },
-//                            modifier = Modifier
-//                                .size(48.dp)
-//                                .weight(1f)
-//                        ) {
-//                            Icon(
-//                                imageVector = Icons.Filled.Home,
-//                                contentDescription = "Home"
-//                            )
-//                        }
-//                        IconButton(
-//                            onClick = { },
-//                            modifier = Modifier
-//                                .size(48.dp)
-//                                .weight(1f)
-//                        ) {
-//                            Icon(
-//                                imageVector = Icons.Filled.AccountCircle,
-//                                contentDescription = "Account"
-//                            )
-//                        }
-//                    }
-//                },
-//                containerColor = MaterialTheme.colorScheme.primaryContainer,
-//
-//            )
-//        },
-        content = { paddingValues ->
-//            MyApplication( modifier = Modifier.padding(paddingValues))
         }
-    )
+    ) {padding ->
+        ScreenContent(Modifier.padding(padding))
+        }
+
 }
+
+@Composable
+fun ScreenContent(modifier: Modifier){
+    Column(
+        modifier = modifier.fillMaxSize(),
+
+    ) {
+        ListItem()
+    }
+}
+
+@Composable
+fun ListItem() {
+
+    Card(
+        onClick = {},
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+//            backgroundColor = Color(0xFF1E1E1E)
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Column(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(text = "WOi", style = MaterialTheme.typography.titleLarge)
+                Text(text = "Jawir", style = MaterialTheme.typography.titleMedium)
+                Text(
+                    text = "Ireng",
+                )
+            }
+            IconButton(onClick = { /* Edit action */ }) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit"
+                )
+            }
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
--- app/src/main/java/org/d3if/infoker/ui/screen/perusahaan/tabs/ProfilScreen.kt
+++ app/src/main/java/org/d3if/infoker/ui/screen/perusahaan/tabs/ProfilScreen.kt
@@ -1,19 +1,163 @@
-package org.d3if.infoker.ui.screen.perusahaan.tabs
+package org.d3if.infoker.ui.screen.user
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircleOutline
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.d3if.infoker.ui.theme.InfokerTheme
 
 @Composable
-fun ProfileScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(
-            text = "Profil",
-            style = typography.titleLarge,
+fun ProfilScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+        , horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HeaderCompany()
+        BeforePersonalCompany()
+        PersonalCompany()
+    }
+}
+
+@Composable
+fun HeaderCompany() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.primaryContainer,
+//            shape = RoundedCornerShape(bottomEnd = 20.dp, bottomStart = 20.dp)
+            )
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(64.dp)
+                .background(Color.Gray, shape = CircleShape)
+
         )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column {
+            Text(
+                text = "Jawir",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+            Text(text = "jawir@email")
+        }
+    }
+}
+
+@Composable
+fun PersonalCompany() {
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Deskripsi Perusahaan",
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+            )
+            IconButton(onClick = { /* Edit action */ }) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit",
+                )
+            }
+        }
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+//            backgroundColor = Color(0xFF1E1E1E)
+        ) {
+            Text(
+                text = "P",
+                color = Color.Gray,
+                modifier = Modifier.padding( 16.dp)
+            )
+        }
+
+    }
+}
+
+// tampilan sebelum klik tombol tambah
+@Composable
+fun BeforePersonalCompany() {
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Deskripsi Perusahaan",
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+            )
+
+        }
+        Text(
+            text = "Beritahu Pegawai tentang perusahaan anda",
+            color = Color.Gray,
+            modifier = Modifier.padding( 4.dp)
+        )
+        OutlinedButton(
+            onClick = { /*Tambah pendidikan*/ },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = "Masukan Deskripsi")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreenPreview() {
+    InfokerTheme {
+        ProfilScreen()
     }
 }
